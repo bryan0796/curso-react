e79fb57ce172dd9987bf3755c888f88be1e8b3f5
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-06-20 17:59:56 -0300

    introducao-router

---
+++ 5-React-Router/Router/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      Esse é o <strong>Header</strong>
+    </div>
+  )
+}
+
+export default Header
---
+++ 5-React-Router/Router/src/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <p>Erro <strong>404</strong>, página não encontrada</p>
+    </div>
+  )
+}
+
+export default NotFound;
