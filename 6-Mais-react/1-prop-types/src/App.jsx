414db91afbafa32f5a877e896938c81a447df356
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-07-04 19:55:14 -0300

    prop-types

---
+++ 6-Mais-react/1-prop-types/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+  return <div>
+    <Button width={200} >Clique aqui</Button>
+  </div>;
+};
+
+export default App;
