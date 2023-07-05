d081c0694b8a46d57dac4e2ca709d69604e4a125
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-07-05 16:15:40 -0300

    useMemo

--- 6-Mais-react/3-use-memo/src/App.jsx
+++ 6-Mais-react/3-use-memo/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 const App = () => {
-  return <div>Meu app</div>;
+  const [contar, setContar] = React.useState(0)
+  return <div>
+    <Header />
+    <button onClick={() => setContar(contar + 1)} >{contar}</button>
+  </div>;
 };
 
 export default App;
